7356e446005aafaa1af7706a29b1535d4fd0ce6b
L A Minora <leonardo.minora@gmail.com>
2024-08-02 16:04:21 -0300

    introdução a eventos no react - tutorial tictactoe

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,48 @@
-const Square = ({value}) => {
-  const valor = value ?? "-";
+import { useState } from "react";
 
-// const Square = (props) => {
-//   console.info(props);
-//   const valor = props.value ?? "-";
+const Square = ({ value }) => {
+  const [valor, mudarValor] = useState(value ?? "-");
+  // const [valor] = useState(value ?? "-");
+  // let valor = value ?? "-";
+  // const valor = value ?? "-";
 
-  // const valor = props.value ? props.value : "-";
-  // let valor;
-  // if (props.value) {
-  //   valor = props.value;
-  // } else {
-  //   valor = "-";
-  // }
+  const escutarClique = (evento) => {
+    console.info(evento)
+    // valor = "X";
+    mudarValor("X");
+  };
+
   return (
-    <button className="square">
-      {valor}
-    </button>
+    <button className="square" onClick={escutarClique} >
+      { valor }
+    </button >
   );
 };
 
-const Aplicacao = () => {
+const Board = () => {
   return (
     <>
       <div className="board-row">
-        <Square value="X" />
-        <Square value="0" />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
 };
 
 // const Aplicacao = (<button>X</button>);
+
+const Aplicacao = () => <Board />;
 
 export default Aplicacao;
